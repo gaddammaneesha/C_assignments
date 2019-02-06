5ed23e115876bbcc006a9ecadc4fa71b048a32e8
maneesha <gaddammaneesha2@gmail.com>
2019-02-05 16:00:01 -0800

    counting digits,characters,blanks and tabs

---
+++ Assignment1/count/count.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<ctype.h>
+int main()
+{
+	char text[81];
+	printf("enter string\n");
+	gets(text);
+	int dig_count = 0, alpha_count = 0, blank_count = 0, tab_count = 0, i = 0;
+	while (text[i] != '\0')
+	{
+		if (isalpha(text[i]))
+			alpha_count++;
+		else if (isdigit(text[i]))
+			dig_count++;
+		else if (text[i] ==' ')
+			blank_count++;
+		else
+			tab_count++;
+		i++;
+	}
+	printf("no.of aplhabets are %d", alpha_count);
+	printf("no.of digits are %d", dig_count);
+	printf("no.of blank spaces are %d", blank_count);
+	printf("no.of tabs are %d", tab_count);
+	return 0;
+}
