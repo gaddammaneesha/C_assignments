62cbb24a75049b84c10289b540e0ce94a1430fc5
gaddammaneesha <gaddammaneesha2@gmail.com>
2019-02-07 12:10:25 -0800

    rotating number

--- Assignment1/rotate right of number/rotaterightofnum.c
+++ Assignment1/rotate right of number/rotaterightofnum.c
@@ -2,26 +2,24 @@
 #include<math.h>
 int main()
 {
-	int n, count = 0, r, dig, res;
+	int n, count = 0, r, res, t;
 	printf("enter number to be rotated:\n");
 	scanf_s("%d", &n);
-	int t = n;
+	t = n;
 	printf("enter no.of rotations:\n");
 	scanf_s("%d", &r);
 	while (n != 0)
 	{
-		int rem = n % 10;
 		n = n / 10;
 		count++;
 	}
-
-	while (r>0)
-	{
-		res = (t % 10)*pow(10, (count - 1)) + (t / 10);
+	while(r>0)
+	{ 
+			res = (t % 10)*(int)pow(10, (count - 1)) + (t / 10);
+		t = res;
 		r--;
-
 	}
 	printf("number after rotations:%d", res);
-	//rttyr
+	//scanf_s("%d", &r);
 	return 0;
 }
