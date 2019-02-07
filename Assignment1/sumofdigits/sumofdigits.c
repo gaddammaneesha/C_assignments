1a35a52da346ba8879431588e8896982c7a8214f
gaddammaneesha <gaddammaneesha2@gmail.com>
2019-02-06 17:15:11 -0800

    sum of digits

---
+++ Assignment1/sumofdigits/sumofdigits.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int n, r, sum = 0;
+	printf("Enter the number:");
+	scanf_s("%d", &n);
+	while (n > 0)
+	{
+		r = n % 10;
+		sum += r;
+		n /= 10;
+	}
+	printf("Sum of digits is:%d", sum);
+	return 0;
+}
