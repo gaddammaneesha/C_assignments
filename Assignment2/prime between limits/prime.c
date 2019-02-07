7b51bb3eb20a973c21159a6e810bb73dd59ee5cc
gaddammaneesha <gaddammaneesha2@gmail.com>
2019-02-06 16:44:55 -0800

    prime

---
+++ Assignment2/prime between limits/prime.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+	int low, high, i, j, count = 0;
+	printf("enter the low limit\n");
+	scanf_s("%d", &low);
+	printf("enter the high limit\n");
+	scanf_s("%d", &high);
+	printf("prime numbers between %d and %d are :", low, high);
+	for (i = low; i <= high; i++)
+	{
+		count = 0;
+		for (j = 1; j <= i; j++)
+		{
+			if (i%j == 0)
+				count++;
+		}
+		if (count == 2)
+			printf("%d  ", i);
+	}
+	
+	return 0;
+}
+
