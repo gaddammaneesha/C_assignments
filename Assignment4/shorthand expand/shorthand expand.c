873c96c014ac185ea3b0b676f7a0a73acec6d52e
pranitha <pranitha285@gmail.com>
2019-02-09 20:56:42 +0530

    shorthand expand

---
+++ Assignment4/shorthand expand/shorthand expand.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include<ctype.h>
+void expand(char *p, char *q)
+{
+	int i = 0, j = 0, k, l;
+	while (*(p + i) != '\0')
+	{
+		if (*(p + i) != '-')
+		{
+			*(q + j) = *(p + i);
+			j++;
+		}
+		else
+		{
+			if (isalpha(*(p + (i - 1))))
+			{
+				k = (int)(*(p + (i - 1)));
+				l = (int)(*(p + (i + 1)));
+				while (k<l - 1)
+				{
+					*(q + j) = (char)(++k);
+					j++;
+				}
+			}
+			else
+			{
+				k = (*(p + (i - 1)));
+				l = (*(p + (i + 1)));
+				while (k<l - 1)
+				{
+					*(q + j) = (char)(++k);
+					j++;
+				}
+			}
+
+		}
+		i++;
+	}
+	*(q + j) = '\0';
+	printf("string %s after expanding shorthand notation is %s", p, q);
+	gets();
+}
+int main()
+{
+	char s1[50], s2[50];
+	int ind;
+	printf("enter string\n");
+	gets(s1);
+	expand(s1, s2);
+	return 0;
+}
+
+
+
+
