da0262d1632d80fb2566604cf65ee53919cd12de
maneesha <gaddammaneesha2@gmail.com>
2019-02-09 21:46:32 +0530

    analysing

---
+++ Assignment4/analaysing/analaysing 2d chararray.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	float table[2][3] = { { 1.1,1.2,1.3 },{ 2.1,2.2,2.3 } };
+	printf("%ld\n",(table+1));
+	printf("%ld\n", *(table + 1));
+	printf("%ld\n", (*(table + 1) + 1));
+	printf("%ld\n", (*(table)+1)  );
+	printf("%f\n", *(*(table + 1) + 1));
+	printf("%f\n", *(*(table)+1));
+	printf("%f\n", *(*(table + 1)));
+	printf("%f\n", *(*(table)+1) + 1);
+	return 0;
+}
